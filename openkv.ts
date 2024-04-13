6f1574cae5061115c8a8ff48bf34bc79e26ffe25
Sander Spilleman <sander@spilleman.nl>
2024-04-13 09:36:58 +0200

    openkv

--- openkv.ts
+++ openkv.ts
@@ -1,8 +1,8 @@
 import { GP } from "./interfaces.ts";
 
-const formule1 =
-  "https://api.deno.com/databases/59849518-4816-42e8-84cf-4a0609d33c93/connect";
-export const kv = await Deno.openKv(formule1);
+// const formule1 = "https://api.deno.com/databases/59849518-4816-42e8-84cf-4a0609d33c93/connect";
+// export const kv = await Deno.openKv(formule1);
+export const kv = await Deno.openKv();
 
 export const getGPS = async (year: number) => {
   const entries = kv.list<GP>({ prefix: ["gps", year] });
